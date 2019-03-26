6bef60fae30f11e771fa39b84ecc6370ea4fed6c
Brad Mortensen <b.mort86@gmail.com>
2019-03-26 13:48:24 -0600

    adds rps.py test cases

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -5,7 +5,15 @@ import sys
 
 def rock_paper_scissors(n):
     options = ['rock', 'paper', 'scissors']
-
+    output = []
+    if n <= 0:
+        return [output]
+    elif n == 1:
+        for option in options:
+            output.append([option])
+    return output
+    else:
+        return output
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         num_plays = int(sys.argv[1])
